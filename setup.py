0d33312debb9dbafef7874a3d7491aaa64202689
Kiril Savino <aurumaeus@gmail.com>
2010-07-27 23:12:02 -0400

    increment sub-minor version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "mango",
-    version = "0.1",
+    version = "0.1.1",
     description = "Django session and auth backends using MongoDB",
     author = "Vinay Pulim",
     license = "BSD License",
